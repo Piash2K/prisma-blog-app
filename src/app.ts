a99670b721bb753aaa6f67e3059c481ed73e58ad
Piash Islam <piashislam2k@gmail.com>
2026-02-19 12:34:13 +0600

    implementing create post API part-1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from "express";
+import { postRouter } from "./modules/post/post.router";
 
 const app: Application = express();
+
+app.use(express.json());
+
+app.use("/posts", postRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Prisma blog app server running");
