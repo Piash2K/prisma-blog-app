efd21f2871d0277431341c65181ed1eac705d861
Piash Islam <piashislam2k@gmail.com>
2026-02-19 12:47:11 +0600

    implementing create post API part-2

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { PostService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
-  console.log({ req, res });
+  try {
+    const result = await PostService.createPost(req.body);
+    res.status(201).json(result);
+  } catch (e) {
+    res.status(400).json({
+        error: "Post Creation Failed",
+        details: e
+    })
+  }
 };
 
 export const PostController = {
