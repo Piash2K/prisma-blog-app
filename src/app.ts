4ce53b133883c798f2781d4d5a1ec8123889bfef
Piash Islam <piashislam2k@gmail.com>
2026-02-20 10:51:10 +0600

    email and password authentication (better auth setup)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import { postRouter } from "./modules/post/post.router";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app: Application = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
