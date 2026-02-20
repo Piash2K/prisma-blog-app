eca16e4b2da94317f53ffdcfbb79d7564436d7f3
Piash Islam <piashislam2k@gmail.com>
2026-02-20 11:10:30 +0600

    extending the user model: adding aditional user fields

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,33 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-
-
+import { role } from "better-auth/plugins";
+import { stat } from "node:fs";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    emailAndPassword: { 
-    enabled: true, 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  trustedOrigins: [process.env.APP_URL || "http://localhost:3000"],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        default: "user",
+        required: false,
+      },
+      phone:{
+        type: "string",
+        required: false,
+      },
+      status:{
+        type: "string",
+        default: "active",
+        required: false,
+      }
+    },
+    emailAndPassword: {
+      enabled: true,
+    },
   },
 });
